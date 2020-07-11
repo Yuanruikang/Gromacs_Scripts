a7be23e56312a8a259d047d263a97f8683507087
Yuanruikang <yuanstarsun@gmail.com>
2020-07-11 21:14:43 +0800

    0714

--- primary_analysis_trajectory.sh
+++ primary_analysis_trajectory.sh
@@ -35,7 +35,7 @@ echo -e "Box-X\nBox-Y\nBox-Z" | gmx energy -f ${name}.edr -o energy_box.xvg
 
 xmgrace -block box.xvg -bxy 1:2  -bxy 1:3 -bxy 1:4
 #check the minimum mirror
-gmx mindist -f ${name}.trr -s ${name}.tpr -od minimal-periodic-distance.xvg -pi
+echo 1 | gmx mindist -f ${name}.trr -s ${name}.tpr -od minimal-periodic-distance.xvg -pi -tu ns
 
 #adjust the trajectory
 
@@ -51,35 +51,28 @@ sed '/TIP3/d;/POT/d' step6.6_equilibration.gro  > ${name}_nowater.gro   # 删除
 
 gmx make_ndx -f ${name}.gro -o index_pbc.ndx -n index.ndx
 
-gmx trjconv -s ${name}.tpr -f ${name}.trr -o ${name}_noPBC.xtc -center -pbc mol -n index_pbc.ndx
+echo -e "MEMB \n system" | gmx trjconv -s ${name}.tpr -f ${name}.trr -o ${name}_noPBC.xtc -center -pbc mol -n index_pbc.ndx
 
-#gmx trjconv -s ${name}.tpr -f ${name}_noPBC.xtc -o ${name}_nojump.xtc -pbc nojump -ur compact -center
+echo 1 0 | gmx trjconv -f ${name}_noPBC.xtc -s  ${name}.tpr -fit rot+trans -o ${name}_noPBC_fit.xtc -n index_pbc.ndx
+
+frame=200000
+echo 0 | gmx trjconv -f ${name}_noPBC_fit.xtc -s  ${name}.tpr -o ${name}_final_frame.gro -dump $frame -sep -n index_pbc.ndx
+
+
 #Gyrate
-gmx gyrate -s ${name}.tpr -f ${name}_noPBC.xtc -o gyrate.xvg
+echo 1 | gmx gyrate -s ${name}.tpr -f ${name}_noPBC.xtc -o gyrate.xvg
 
 #RMSF
 gmx rmsf -f ${name}.trr -s ${name}.tpr -o rmsf-per-residue.xvg -ox average.pdb -oq bfactors-residue.pdb -res
 
 #Comparison with the initial structure
 
-gmx rms -s ${name}.tpr -f ${name}_noPBC.xtc -o rmsd_all_atoms_vs_start.xvg -tu ns
+echo 1 1 | gmx rms -s ${name}.tpr -f ${name}_noPBC.xtc -o rmsd_all_atoms_vs_start.xvg -tu ns
 
-gmx rms -s ${name}.tpr -f ${name}_noPBC.xtc -o rmsd_backbone_vs_start.xvg -tu ns
+echo 4 4 | gmx rms -s ${name}.tpr -f ${name}_noPBC.xtc -o rmsd_backbone_vs_start.xvg -tu ns
 
 echo "Protein" | gmx trjconv -f ${name}_noPBC.xtc  -s ${name}.tpr -o traj_protein_noPBC.xtc
 
-gmx rms -f traj_protein_noPBC.xtc -s average.pdb -o rmsd-all-atom-vs-average.xvg
+gmx rms -f traj_protein_noPBC.xtc -s average.pdb -o rmsd-all-atom-vs-average.xvg -tu ns
 
-gmx rms -f traj_protein_noPBC.xtc -s average.pdb -o rmsd-backbone-vs-average.xvg
-
-#analysis the membrane
-#PO4
-a P | a O11 | a O12 | a O13 | a O14
-gmx distance -s ${name}.tpr -f ${name}_noPBC.xtc -select  'cog of group "MEMB" plus cog of group "PROT"' -oav Distance_PROT_av.xvg -oall Distance_PROT_all.xvg -n index_pbc.ndx
-gmx distance -s ${name}.tpr -f ${name}_noPBC.xtc -select  'cog of group "MEMB" plus cog of group "PO4"' -oav Distance_PO4_av.xvg -oall Distance_PO4_all.xvg -n index_pbc.ndx
-#N(CH3)3
-a N | a C13 | a H13A | a H13B | a H13C | a C14 | a H14A | a H14B | a H14C | a C15 | a H15A | a H15B | a H15C
-gmx distance -s ${name}.tpr -f ${name}_noPBC.xtc -select  'cog of group "MEMB" plus cog of group "NCH33"' -oav Distance_NCH33_av.xvg -oall Distance_NCH33_all.xvg -n index_pbc.ndx
-#Headgroups
-pymol 
-show sticks, name C1+C2+C3+N4+C5+C6+O7+P8+O9+O10+O11
+gmx rms -f traj_protein_noPBC.xtc -s average.pdb -o rmsd-backbone-vs-average.xvg -tu ns
